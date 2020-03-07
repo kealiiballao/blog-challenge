5b9aeaa125049b2ee85c296c559b0566fc38449c
Kealii-SurfacePro <ballao@hawaii.edu>
2020-03-06 19:03:24 -1000

    Installed on Third Computer

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const http = require("http");
 const lodash = require("lodash");
-let posts = [];
+const posts = [];
 const homeStartingContent =
   "Home Page: Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutStartingContent =
@@ -17,30 +17,41 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 //Render the home page.
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   let startingContent = homeStartingContent;
-  res.render("home", { textContent: startingContent, posts: posts });
+  res.render("home", {
+    textContent: startingContent,
+    posts: posts
+  });
 });
 
-app.get("/post", function(req, res) {
-  res.render("post");
+app.get("/posts", function (req, res) {
+  res.render("posts", {
+    posts: posts
+  });
 });
 
-app.get("/about", function(req, res) {
+app.get("/about", function (req, res) {
   let startingContent = aboutStartingContent;
-  res.render("about", { textContent: startingContent });
+  res.render("about", {
+    textContent: startingContent
+  });
 });
-app.get("/contact", function(req, res) {
+app.get("/contact", function (req, res) {
   let startingContent = contactStartingContent;
-  res.render("contact", { textContent: startingContent });
+  res.render("contact", {
+    textContent: startingContent
+  });
 });
-app.get("/compose", function(req, res) {
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
-app.post("/compose", function(req, res) {
+app.post("/compose", function (req, res) {
   const post = {
     title: req.body.postTitle,
     content: req.body.postContent
@@ -48,16 +59,18 @@ app.post("/compose", function(req, res) {
   posts.push(post);
   res.redirect("/");
 });
-app.get("/posts/:postName", function(req, res) {
+app.get("/posts/:postName", function (req, res) {
   const reqTitle = req.params.postName;
-  posts.forEach(function(post) {
+  posts.forEach(function (post) {
     const storedTitle = post.title;
-   if (storedTitle === reqTitle){console.log("match found")}else{
-    console.log("match not found");
-  }
-  
+    if (storedTitle === reqTitle) {
+      console.log("match found")
+    } else {
+      console.log("match not found");
+    }
+
   });
 });
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
